a236a87ae4127f71211232c0f69a036b6a23d74d
1f604 <1f604@users.noreply.github.com>
2021-07-28 11:34:51 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"log"
-	"main/util"
 	"runtime"
 	"time"
 )
 
-var TCP_port = 12345
-var logfilepath = "/tmp/logs/log.txt"
+var (
+	TCP_port                           = 12345
+	rlog                 *RotateWriter = NewRotateWriter(log_directory)
+	log_directory                      = "/tmp/logs/log.txt"
+	log_rotationinterval               = 30 * time.Second
+)
 var g_count = 0
 
 func foo() {
 	g_count++
-	log.Println("foo was executed: ", g_count)
+	rlog.println("foo was executed: ", g_count)
 	if g_count < 3 || g_count > 10 && g_count < 16 {
 		time.Sleep(4 * time.Second)
 		runtime.GC()
@@ -21,11 +23,11 @@ func foo() {
 }
 
 func main() {
-	l := util.CheckTCPPort(TCP_port)
-	f := util.SetLogFile(logfilepath)
-	defer util.Use(l, f) // need this otherwise GC will close TCP port.
+	l := CheckTCPPort(TCP_port)
+	go rlog.LogRotater(log_rotationinterval)
+	defer Use(l) // need this otherwise GC will close TCP port.
 
-	go util.Retryfunc("func foo", foo, 3*time.Second, 3*time.Second)
-	go util.Retryproc("../sleep/sleep1", 3*time.Second, 3*time.Second)
+	go Retryfunc("func foo", foo, 3*time.Second, 3*time.Second)
+	//go Retryproc("../sleep/sleep1", 3*time.Second, 3*time.Second)
 	time.Sleep(100 * time.Hour)
 }
