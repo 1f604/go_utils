1c408655e6dae29209729ed1e12fec39e05beb59
1f604 <1f604@users.noreply.github.com>
2021-07-28 11:39:41 +0100

    revert

--- util.go
+++ util.go
@@ -1,7 +1,8 @@
-package main
+package util
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"os/exec"
@@ -55,12 +56,12 @@ func Retryfunc(taskname string, dotask retrylib_task, expected_duration time.Dur
 		start := time.Now()
 		print_log := !count.maxValReached()
 		if print_log {
-			rlog.println("launching " + taskname + " ...")
+			log.Printf("launching %s ...\n", taskname)
 		}
 		dotask()
 		duration := time.Since(start)
 		if print_log {
-			rlog.Printf("%s finished after %d seconds.\n", taskname, duration/time.Second)
+			log.Printf("%s finished after %d seconds.\n", taskname, duration/time.Second)
 		}
 		if duration > expected_duration {
 			count.zero()
@@ -68,7 +69,7 @@ func Retryfunc(taskname string, dotask retrylib_task, expected_duration time.Dur
 			count.incr()
 		}
 		if print_log {
-			rlog.Printf("%s: sleeping for %d seconds before re-running\n", taskname, count.getValue()/time.Second)
+			log.Printf("%s: sleeping for %d seconds before re-running\n", taskname, count.getValue()/time.Second)
 		}
 		time.Sleep(count.getValue())
 	}
@@ -84,12 +85,12 @@ func RetryprocWithArgs(taskname string, procname string, args []string, expected
 
 func Pkill(pname string) {
 	cmd := exec.Command("pkill", pname)
-	rlog.println("killing", pname)
+	log.Println("killing", pname)
 	err := cmd.Run()
 	if err != nil {
-		rlog.println("error:", err)
+		log.Println("error:", err)
 	} else {
-		rlog.println("done.")
+		log.Println("done.")
 	}
 }
 
@@ -98,13 +99,13 @@ func Retryproc(procname string, expected_duration time.Duration, max_wait time.D
 		cmd := exec.Command(procname)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		rlog.Printf("launching process %s ...\n", procname)
+		log.Printf("launching process %s ...\n", procname)
 		err := cmd.Run()
 
 		if err != nil {
-			rlog.Printf("process %s: an error occurred: %v\n", procname, err)
+			log.Printf("process %s: an error occurred: %v\n", procname, err)
 		} else {
-			rlog.Printf("process %s completed without error.\n", procname)
+			log.Printf("process %s completed without error.\n", procname)
 		}
 	}
 	Retryfunc("command "+procname, f, expected_duration, max_wait)
@@ -115,10 +116,22 @@ func CheckTCPPort(port int) net.Listener {
 	var l net.Listener
 	var err error
 	if l, err = net.Listen("tcp", ":"+fmt.Sprint(port)); err != nil {
-		rlog.Fatal("an instance was already running")
+		log.Fatal("an instance was already running")
 	}
 	fmt.Println("single instance started.")
 	return l
+}
+
+// https://stackoverflow.com/questions/19965795/how-to-write-log-to-file
+func SetLogFile(filename string) *os.File {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening log file: %v", err)
+	}
+
+	log.SetOutput(f)
+	log.Println("Started logging.")
+	return f
 }
 
 // https://stackoverflow.com/questions/21743841/how-to-avoid-annoying-error-declared-and-not-used
@@ -126,90 +139,4 @@ func Use(vals ...interface{}) {
 	for _, val := range vals {
 		_ = val
 	}
-}
-
-// from https://stackoverflow.com/a/28797984
-type RotateWriter struct {
-	sync.Mutex
-	filename string // should be set to the actual filename
-	fp       *os.File
-}
-
-// Make a new RotateWriter. Return nil if error occurs during setup.
-func NewRotateWriter(filename string) *RotateWriter {
-	w := &RotateWriter{filename: filename}
-	err := w.Rotate()
-	if err != nil {
-		panic("WRITER NOT CREATED! CHECK THE LOG FILE DIRECTORY EXISTS!")
-	}
-	return w
-}
-
-// Write satisfies the io.Writer interface.
-func (w *RotateWriter) Write(output []byte) (int, error) {
-	w.Lock()
-	defer w.Unlock()
-	return w.fp.Write(output)
-}
-
-// Perform the actual act of rotating and reopening file.
-func (w *RotateWriter) Rotate() (err error) {
-	w.Lock()
-	defer w.Unlock()
-
-	// Close existing file if open
-	if w.fp != nil {
-		err = w.fp.Close()
-		w.fp = nil
-		if err != nil {
-			return
-		}
-	}
-	// Rename dest file if it already exists
-	_, err = os.Stat(w.filename)
-	if err == nil {
-		err = os.Rename(w.filename, w.filename+"."+time.Now().Format(time.RFC3339))
-		if err != nil {
-			return
-		}
-	}
-
-	// Create a file.
-	w.fp, err = os.Create(w.filename)
-	return
-}
-
-// Rotates the file every x nanoseconds.
-func (w *RotateWriter) LogRotater(x time.Duration) {
-	ticker := time.NewTicker(x)
-	for range ticker.C {
-		w.Rotate()
-	}
-}
-
-func (w *RotateWriter) Print(args ...interface{}) (int, error) {
-	datestring := time.Now().Format("2006-01-02 15:04:05")
-	output := []interface{}{datestring + " "}
-	output = append(output, args...)
-	str := fmt.Sprint(output...)
-	return w.Write([]byte(str))
-}
-
-func (w *RotateWriter) Printf(formatstr string, args ...interface{}) (int, error) {
-	formattedstr := fmt.Sprintf(formatstr, args...)
-	datestring := time.Now().Format("2006-01-02 15:04:05")
-	output := datestring + " " + formattedstr
-	return w.Write([]byte(output))
-}
-
-func (w *RotateWriter) println(vals ...interface{}) (int, error) {
-	vals = append(vals, "\n")
-	return w.Print(vals...)
-}
-
-func (w *RotateWriter) Fatal(vals ...interface{}) {
-	output := []interface{}{"FATAL: "}
-	output = append(output, vals...)
-	w.println(output...)
-	os.Exit(1)
 }
